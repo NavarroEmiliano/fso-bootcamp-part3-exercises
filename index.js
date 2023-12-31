216b71e54d1f13736e6f16be071d69458aaa22e3
Emiliano Navarro <emilianonavarro1993@gmail.com>
2023-12-31 02:43:29 -0300

    Exercise 3.22.

--- index.js
+++ index.js
@@ -1,96 +1,95 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/person");
+/* eslint-disable no-unused-vars */
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("dist"));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-app.get("/api/persons", async (request, response, next) => {
+app.get('/api/persons', async (request, response, next) => {
   try {
-    const result = await Person.find({});
+    const result = await Person.find({})
     if (result.length) {
-      response.json(result);
+      response.json(result)
     } else {
-      throw new Error("Base de datos vacía.");
+      throw new Error('Base de datos vacía.')
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.code === 11000) {
     return response.status(409).send({
       error:
-        "The resource already exists in the database. Duplicates are not allowed.",
-    });
-  } else if (error.name === "ValidationError") {
-    return response
-      .status(400)
-      .json({
-        error: error.errors.number
-          ? error.errors.number.message
-          : error.errors.name.message,
-      });
+        'The resource already exists in the database. Duplicates are not allowed.',
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.errors.number
+        ? error.errors.number.message
+        : error.errors.name.message,
+    })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.get("/api/persons/:id", async (request, response, next) => {
+app.get('/api/persons/:id', async (request, response, next) => {
   try {
-    const id = request.params.id;
-    const person = await Person.findById(id);
+    const id = request.params.id
+    const person = await Person.findById(id)
     if (person) {
-      response.json(person);
+      response.json(person)
     } else {
-      response.status(404).end;
+      response.status(404).end
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.get("/info", async (request, response, next) => {
+app.get('/info', async (request, response, next) => {
   try {
-    const persons = await Person.find({});
-    const numbers = persons.length;
-    const date = new Date();
+    const persons = await Person.find({})
+    const numbers = persons.length
+    const date = new Date()
     response.send(`<h3>Phonebook has info for ${numbers} people</h3>
-    <h3>${date}</h3>`);
+    <h3>${date}</h3>`)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.delete("/api/persons/:id", async (request, response, next) => {
+app.delete('/api/persons/:id', async (request, response, next) => {
   try {
-    const id = request.params.id;
-    const deletedPerson = await Person.findByIdAndDelete(id);
-    if (deletedPerson) response.status(204).end();
+    const id = request.params.id
+    const deletedPerson = await Person.findByIdAndDelete(id)
+    if (deletedPerson) response.status(204).end()
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.put("/api/persons/:id", async (request, response, next) => {
+app.put('/api/persons/:id', async (request, response, next) => {
   try {
-    const body = request.body;
+    const body = request.body
     const person = {
       name: body.name,
       number: body.number,
-    };
+    }
 
     const updatedPerson = await Person.findByIdAndUpdate(
       request.params.id,
@@ -98,35 +97,35 @@ app.put("/api/persons/:id", async (request, response, next) => {
       {
         new: true,
       }
-    );
+    )
 
     if (updatedPerson) {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     } else {
-      response.status(404).json({ error: "Person not found" });
+      response.status(404).json({ error: 'Person not found' })
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
   const personToSave = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   personToSave
     .save()
     .then((personSaved) => personSaved.toJSON())
     .then((savedAndFormattedPerson) => response.json(savedAndFormattedPerson))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running in port ${PORT}`);
-});
+  console.log(`Server running in port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,21 +1,22 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+/* eslint-disable no-undef */
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
   .then(() => {
-    console.log("Conectado a la base de datos");
+    console.log('Conectado a la base de datos')
   })
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error))
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: [3, "The name must be at least 3 characters."],
+    minlength: [3, 'The name must be at least 3 characters.'],
     required: true,
     unique: true,
   },
@@ -23,21 +24,21 @@ const personSchema = new mongoose.Schema({
     type: Number,
     validate: {
       validator: (number) => {
-        const stringNumber = number.toString();
-        return stringNumber.length >= 8;
+        const stringNumber = number.toString()
+        return stringNumber.length >= 8
       },
-      message: "The number must be at least 8 digits.",
+      message: 'The number must be at least 8 digits.',
     },
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
